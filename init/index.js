7dbed237cd1122e48f5f7e97d9017901f4b47973
mohammed <mohammedsuhail95145@gmail.com>
2026-09-23 21:39:53 +0530

    Implemented Authentication, Authorization and Phase2 is closed.

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({...obj, owner: "6aaf49874f61db75f4066455"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
--- routes/review.js
+++ routes/review.js
@@ -1,42 +1,39 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body); 
-   
-  if(error){
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  }else{
-    next();
-  }
-};
+const { validateReview, isLoggedIn, isAuthor } = require("../middleware.js");
 
 //Review Route
-router.post("/", validateReview, wrapAsync( async (req, res) => {
+router.post("/", 
+  isLoggedIn,
+  validateReview, 
+  wrapAsync( async (req, res) => {
   let listing = await Listing.findById(req.params.id);
   const newReview = new Review(req.body.review);
+  newReview.author = req.user._id;
 
   listing.reviews.push(newReview);
 
   await listing.save();
   await newReview.save();
 
+  req.flash("success", "New Review Created!");
   res.redirect(`/listings/${ listing._id }`);
 }));
 
 //Delete Review Route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId", 
+  isLoggedIn,
+  isAuthor,
+  wrapAsync(async (req, res) => {
   let {id, reviewId} = req.params;
   
   await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
   await Review.findByIdAndDelete(reviewId);
 
+  req.flash("success", "Review Deleted!");
   res.redirect(`/listings/${id}`);
 }));
 
